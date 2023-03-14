3b8321ea13c1134a98243e7f6f7215449bfda402
Akash Singh <akashsingh777567.com>
2023-03-14 19:46:20 +0530

    add intro

--- src/components/Intro/Intro.jsx
+++ src/components/Intro/Intro.jsx
@@ -1,8 +1,54 @@
-import React from 'react'
+import React from 'react';
+import './Intro.css';
+import FloatingDiv from '../FloatingDiv/FloatingDiv';
+import Github from '../../img/github.png';
+import LinkedIn from '../../img/linkedin.png';
+import Instagram from '../../img/instagram.png';
+import Vector1 from '../../img/Vector1.png';
+import Vector2 from '../../img/Vector2.png';
+import Boy from '../../img/boy.png';
+import Crown from '../../img/crown.png';
+import Thumbup from '../../img/thumbup.png';
+import Glassesimoji from '../../img/glassesimoji.png';
 
 const Intro = () => {
   return (
-    <div>Intro</div>
+    <div className="intro">
+        <div className="i-left">
+            <div className="i-name">
+                <span>Hy! I Am</span>
+                <span>Akash Singh</span>
+                <span>Frontend Developer Lorem ipsum dolor sit, amet consectetur adipisicing elit. Tempora necessitatibus assumenda.</span>
+            </div>
+            <button className="button i-button">Hire Me</button>
+            <div className="i-icons">
+                <a href="#">
+                    <img src={Github} alt="" />
+                </a>
+                <a href="#">
+                    <img src={LinkedIn} alt="" />
+                </a>
+                <a href="#">
+                    <img src={Instagram} alt="" />
+                </a> 
+            </div>
+        </div>
+        <div className="i-right">
+            <img src={Vector1} alt="" />
+            <img src={Vector2} alt="" />
+            <img src={Boy} alt="" />
+            <img src={Glassesimoji} alt="" />
+            <div style={{top:'-4%' , left:'68%'}}>
+                <FloatingDiv image={Crown} txt1='Web' txt2='Developer'/>
+            </div>
+            <div style={{top:'18.4rem' , left:'1rem'}}>
+                <FloatingDiv image={Thumbup} txt1='Best Desigh' txt2='Award'/>
+            </div>
+            {/* blur divs */}
+            <div className="blur" style={{background : "rgb(238 210 255)"}}></div>
+            <div className="blur" style={{background : '#c1f5ff', top: '17rem', left: '-9rem', height: '11rem', width: '21rem'}}></div>
+        </div>
+    </div>
   )
 }
 
